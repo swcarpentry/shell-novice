e22aa93637d9ca8aad4657cf1252a541c7a4dea1
Raniere Silva <raniere@ime.unicamp.br>
2015-03-19 00:44:08 -0300

    Get core and gh-pages sync
    This is based on 966a0322695852676884f8c094b83d31cc4c728c from gh-pages.
    
    Thanks to @ChristinaLK to report the problem.

--- tools/check.py
+++ tools/check.py
@@ -671,8 +671,7 @@ LESSON_TEMPLATES = {"index": (IndexPageValidator, "^index"),
                     "discussion": (DiscussionPageValidator, "^discussion")}
 
 # List of files in the lesson directory that should not be validated at all
-SKIP_FILES = ("CONDUCT.md", "CONTRIBUTING.md",
-              "DESIGN.md", "FAQ.md", "LAYOUT.md", "README.md")
+SKIP_FILES = ("DESIGN.md", "FAQ.md", "LAYOUT.md", "README.md")
 
 
 def identify_template(filepath):
