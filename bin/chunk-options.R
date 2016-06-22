9bfebc4d0fc6f75598083734b995eb484fffa512
Francois Michonneau <francois.michonneau@gmail.com>
2016-06-22 10:48:05 -0400

    use .r instead of .source for code chunks

--- bin/chunk-options.R
+++ bin/chunk-options.R
@@ -21,9 +21,9 @@ opts_chunk$set(tidy = FALSE, results = "markup", comment = NA,
 # The hooks below add html tags to the code chunks and their output so that they
 # are properly formatted when the site is built.
 hook_in <- function(x, options) {
-  stringr::str_c("\n\n~~~{.r}\n",
+  stringr::str_c("\n\n~~~\n",
                  paste0(x, collapse="\n"),
-                 "\n~~~\n{: .source}\n\n")
+                 "\n~~~\n{: .r}\n\n")
 }
 
 hook_out <- function(x, options) {
