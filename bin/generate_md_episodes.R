45729b52f9f234e3a28d020428c52203ede6202b
Francois Michonneau <francois.michonneau@gmail.com>
2016-07-20 11:53:47 -0400

    simplify code checking for knitr

--- bin/generate_md_episodes.R
+++ bin/generate_md_episodes.R
@@ -1,8 +1,7 @@
 generate_md_episodes <- function() {
 
-    if (require("knitr") && packageVersion("knitr") < '1.9.19') {
-            stop("knitr must be version 1.9.20 or higher")
-    } else stop("knitr 1.9.20 or above is needed to build the lessons.")
+    if (require("knitr") && packageVersion("knitr") < '1.9.19')
+        stop("knitr must be version 1.9.20 or higher")
 
     if (!require("stringr"))
         stop("The package stringr is required for generating the lessons.")
