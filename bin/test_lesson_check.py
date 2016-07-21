9be4b22988fafd16c05037f6574986725984d533
Greg Wilson <gvwilson@third-bit.com>
2016-07-21 13:35:29 -0400

    Checking future print

--- bin/test_lesson_check.py
+++ bin/test_lesson_check.py
@@ -5,12 +5,12 @@ import util
 
 class TestFileList(unittest.TestCase):
     def setUp(self):
-        self.reporter = util.Reporter(None)  ## TODO: refactor reporter class.
+        self.reporter = util.Reporter()  ## TODO: refactor reporter class.
 
     def test_file_list_has_expected_entries(self):
         # For first pass, simply assume that all required files are present
-        all_filenames = [item[1].replace('%', '')
-                         for item in lesson_check.REQUIRED_FILES]
+        all_filenames = [filename.replace('%', '')
+                         for filename in lesson_check.REQUIRED_FILES]
 
         lesson_check.check_fileset('', self.reporter, all_filenames)
         self.assertEqual(len(self.reporter.messages), 0)
