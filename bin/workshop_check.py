fcc731bf6c88c41900eba1b72a8638d3e6ab4a0e
Raniere Silva <raniere@rgaiacs.com>
2017-11-23 08:20:23 +0000

    Port changes from workshop

--- bin/workshop_check.py
+++ bin/workshop_check.py
@@ -205,9 +205,9 @@ def check_helpers(helpers):
 @look_for_fixme
 def check_email(email):
     """
-    'email' must be a valid email address consisting of characters,
-    an '@', and more characters.  It should not be the default contact
-    email address 'admin@software-carpentry.org'.
+    'email' must be a comma-separated list of valid email addresses.
+    The list may be empty. A valid email address consists of characters,
+    an '@', and more characters.  It should not contain the default contact
     """
 
     return bool(re.match(EMAIL_PATTERN, email)) and \
@@ -286,8 +286,9 @@ HANDLERS = {
                    'helper list isn\'t a valid list of format ' +
                    '["First helper", "Second helper",..]'),
 
-    'email':    (True, check_email,
-                   'contact email invalid or still set to ' +
+    'email':    (True, check_emails,
+                   'contact email list isn\'t a valid list of format ' +
+                   '["me@example.org", "you@example.org",..] or contains incorrectly formatted email addresses or ' +
                    '"{0}".'.format(DEFAULT_CONTACT_EMAIL)),
 
     'eventbrite': (False, check_eventbrite, 'Eventbrite key appears invalid'),
