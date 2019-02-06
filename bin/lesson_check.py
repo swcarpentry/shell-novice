ab8decae6cfafc87040ffec0d387cc9e403756dc
Alex Whan <alexwhan@gmail.com>
2019-02-06 16:26:04 +1100

    lesson_check.py: remove _config.yml from the list of required files
    Pull Request: carpentries/styles#388

--- bin/lesson_check.py
+++ bin/lesson_check.py
@@ -35,7 +35,6 @@ REQUIRED_FILES = {
     '%/README.md': False,
     '%/_extras/discuss.md': True,
     '%/_extras/guide.md': True,
-    '%/_config.yml': True,
     '%/index.md': True,
     '%/reference.md': True,
     '%/setup.md': True,
