cd41c7b84eac8058677df8c3406a26aa8e0c6249
Joao Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2019-12-05 06:47:00 -0600

    Fixes encoding problem on Windows systems, with minimal changes to existing code.

--- bin/util.py
+++ bin/util.py
@@ -117,8 +117,8 @@ def read_markdown(parser, path):
     # Parse Markdown.
     cmd = 'ruby {0}'.format(parser)
     p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
-              close_fds=True, universal_newlines=True)
-    body = str(body.encode('utf-8'))
+              close_fds=True, universal_newlines=True, encoding='utf-8')
+
     stdout_data, stderr_data = p.communicate(body)
     doc = json.loads(stdout_data)
 
