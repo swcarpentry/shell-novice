f419f9cdc6945caef5c8881301ea73c8cc6c7ac9
Maxim Belkin <maxim.belkin@gmail.com>
2021-02-15 14:36:29 -0600

    Fix Kramdown parser crash
    ... by using GFM (GitHub-flavored Markdown) parser (`kramdown-parser-gfm`)
    instead of the default one (`kramdown`).
    
    The default one fails to produce an AST (Abstract Syntax Tree) when
    there is no blank line before the line with the opening code fence.
    
    Related:
     - gettalong/kramdown#530
     - Python-Markdown/markdown#807
    
    Fixes: carpentries/styles#543

--- bin/markdown_ast.rb
+++ bin/markdown_ast.rb
@@ -4,9 +4,10 @@
 # Use Kramdown parser to produce AST for Markdown document.
 
 require 'kramdown'
+require 'kramdown-parser-gfm'
 require 'json'
 
 markdown = $stdin.read
-doc = Kramdown::Document.new(markdown)
+doc = Kramdown::Document.new(markdown, input: 'GFM', hard_wrap: false)
 tree = doc.to_hash_a_s_t
 puts JSON.pretty_generate(tree)
