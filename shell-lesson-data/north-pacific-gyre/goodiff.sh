fe72161da30be3913a902f40ee72c5b0c42bfe55
Trevor Keller <trevor.keller@gmail.com>
2025-08-18 07:47:26 -0400

    Check goo script arguments to prevent data loss. (#1487)
    * Check goo script arguments to prevent data loss.
    
    * Invalid arguments triggering a usage message should return an error code
    
    Co-authored-by: Pariksheet Nanda <pan79@pitt.edu>
    Co-authored-by: Benson Muite <bkmgit@users.noreply.github.com>

--- shell-lesson-data/north-pacific-gyre/goodiff.sh
+++ shell-lesson-data/north-pacific-gyre/goodiff.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+# Incomplete goo comparator by Nelle Nemo.
+
 # check for the right number of input arguments
 if [ $# -ne 2 ]
   then
-    echo "goodiff file1 file2"
-    echo "call goodiff with two arguments"
+    echo "call goodiff with two arguments:"
+    echo "  $0 file1 file2"
     exit 1
 fi
 
+# check read permissions
+if [ ! -f "$1" ]
+then
+    echo "error reading $1"
+    exit 2
+elif [ ! -f "$2" ]
+then
+    echo "error reading $2"
+    exit 2
+fi
 
-# difference of two input files
 # demo version, just return a random number or "files are identical"
 if [ "$1" == "$2" ]
 then
--- shell-lesson-data/north-pacific-gyre/goostats.sh
+++ shell-lesson-data/north-pacific-gyre/goostats.sh
@@ -1,10 +1,26 @@
+#!/bin/bash
+# goostats.sh: a research script by Nelle Nemo
+# Analyze "goo" input data and write "stats" to result file.
+
 # check for the right number of input arguments
 if [ $# -ne 2 ]
-  then
-    echo "goostats file1 file2"
-    echo "call goostats with two arguments"
+then
+    echo "call goostats with two arguments:"
+    echo "  $0 input_file result_file"
     exit 1
 fi
 
+# check if files already exist (good for $1, bad for $2)
+if [ ! -a "$1" ]
+then
+    echo "error reading input: $1"
+    exit 2
+elif [ -a "$2" ]
+then
+    echo "error writing result: $2"
+    exit 2
+fi
+
+# run the numbers
 sleep 2
-head -n 3 $1 | cut -d , -f 1 | sort | uniq > $2
+head -n 3 "$1" | cut -d , -f 1 | sort | uniq > "$2"
